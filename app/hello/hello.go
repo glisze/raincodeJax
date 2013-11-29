452b66a2710f2ab6eb881266dbe7973e9b4725a4
Gunter Liszewski <gunter.liszewski@gmail.com>
2013-11-29 05:43:12 -0800

    test. goapp going.
    working on the Anne Street Resource Centre

--- app/hello/hello.go
+++ app/hello/hello.go
@@ -1,38 +1,29 @@
 package hello
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
+
+    "appengine"
+    "appengine/user"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/get", get)
-	http.HandleFunc("/post", post)
-	http.HandleFunc("/put", put)
-	http.HandleFunc("/delete", delete)
+    http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi, Lo, this is the plat formed app-engine!\n")
-}
-
-func get(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi! What do you want?\n")
-}
-
-func post(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi! Thank You, very, very much! You will hear from us very very; soon. Promise!\n")
-}
-
-func put(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi! Thanks! But what am I going to do with it?\n")
-}
-
-func delete(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi! That was restful. Again!\n")
-}
-
-func main() {
-	fmt.Printf("Hi, I am starting...")
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "Hello, %v!", u)
 }
